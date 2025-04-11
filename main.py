ff0493a78372b7a0b1622bc3326c523e6cc3328f
caleb-sims <caleb.sims@students.fhu.edu>
2025-04-10 20:09:24 -0500

    Created wins counter

--- main.py
+++ main.py
@@ -1,93 +1,111 @@
 import random
 options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+cpu_wins = 0
+user_wins = 0
+play_again = 'Y'
 
+while play_again in ['Y', 'y', 'Yes', 'yes']:
 
-# 1: Take Inputs
-user = input('Enter Rock, Paper, Scissors, Lizard, or Spock:\n')
-cpu = options[random.randint(0,4)]
-
-
-# 2: Check For Invalid Input
-
-# a: Input not in domain
-while user not in options:
-    print('Error: invalid input')
+    # 1: Take Inputs
     user = input('Enter Rock, Paper, Scissors, Lizard, or Spock:\n')
-
-# b: Input equals cpu input
-while cpu == user:
-    print('Player 2 chose', cpu + '.')
-    print('You drew.')
-    user = input('Enter Rock, Paper, Scissors, Lizard, or Spock:\n')
+    cpu = options[random.randint(0,4)]
 
 
-# 3: Create Classes
+    # 2: Check For Invalid Input
 
-class Game():   # parent class
-    def __init__(self):
-        self.wins = []
-        self.loses = []
+    # a: Input not in domain
+    while user not in options:
+        print('Error: invalid input')
+        user = input('Enter Rock, Paper, Scissors, Lizard, or Spock:\n')
 
-class Rock(Game):
-    def __init__(self):
-        super().__init__()
-        self.wins = ['Scissors', 'Lizard']
-        self.loses = ['Paper', 'Spock']
-
-class Paper(Game):
-    def __init__(self):
-        super().__init__()
-        self.wins = ['Rock', 'Spock']
-        self.loses = ['Scissors', 'Lizard']
-
-class Scissors(Game):
-    def __init__(self):
-        super().__init__()
-        self.wins = ['Paper', 'Lizard']
-        self.loses = ['Rock', 'Spock']
-
-class Lizard(Game):
-    def __init__(self):
-        super().__init__()
-        self.wins = ['Paper', 'Spock']
-        self.loses = ['Rock', 'Scissors']
-
-class Spock(Game):
-    def __init__(self):
-        super().__init__()
-        self.wins = ['Scissors', 'Rock']
-        self.loses = ['Paper', 'Lizard']
+    # b: Input equals cpu input
+    while cpu == user:
+        print(f'Player 2 chose {cpu}.')
+        print('You drew.')
+        cpu = options[random.randint(0,4)]
+        user = input('Enter Rock, Paper, Scissors, Lizard, or Spock:\n')
 
 
-# 4: Apply Input to Respective Class
-user_class = 0
+    # 3: Create Classes
 
-if user == 'Rock':
-    user_class = Rock()
-elif user == 'Paper':
-    user_class = Paper()
-elif user == 'Scissors':
-    user_class = Scissors()
-elif user == 'Lizard':
-    user_class = Lizard()
-elif user == 'Spock':
-    user_class = Spock()
+    class Game():   # parent class
+        def __init__(self):
+            self.wins = []
+            self.loses = []
+
+    class Rock(Game):
+        def __init__(self):
+            super().__init__()
+            self.wins = ['Scissors', 'Lizard']
+            self.loses = ['Paper', 'Spock']
+
+    class Paper(Game):
+        def __init__(self):
+            super().__init__()
+            self.wins = ['Rock', 'Spock']
+            self.loses = ['Scissors', 'Lizard']
+
+    class Scissors(Game):
+        def __init__(self):
+            super().__init__()
+            self.wins = ['Paper', 'Lizard']
+            self.loses = ['Rock', 'Spock']
+
+    class Lizard(Game):
+        def __init__(self):
+            super().__init__()
+            self.wins = ['Paper', 'Spock']
+            self.loses = ['Rock', 'Scissors']
+
+    class Spock(Game):
+        def __init__(self):
+            super().__init__()
+            self.wins = ['Scissors', 'Rock']
+            self.loses = ['Paper', 'Lizard']
 
 
-# 5: Find Winner
+    # 4: Apply Input to Respective Class
 
-def get_winner(cpu, wins_list):
-    winner = 0
-    if cpu in wins_list:
-        winner = True
-    elif cpu in wins_list:
-        winner = False
-    return winner
+    user_class = 0
+
+    if user == 'Rock':
+        user_class = Rock()
+    elif user == 'Paper':
+        user_class = Paper()
+    elif user == 'Scissors':
+        user_class = Scissors()
+    elif user == 'Lizard':
+        user_class = Lizard()
+    elif user == 'Spock':
+        user_class = Spock()
 
 
-print(f'Player 2 chose {cpu}.')
+    # 5: Find Winner
 
-if get_winner(cpu, user_class.wins):
-    print('You win!')
-elif not get_winner(cpu, user_class.wins):
-    print('You lose.')
+    def get_winner(cpu, wins_list):
+        winner = 0
+        if cpu in wins_list:
+            winner = True
+        elif cpu in wins_list:
+            winner = False
+        return winner
+
+
+    print(f'Player 2 chose {cpu}.')
+
+    if get_winner(cpu, user_class.wins):
+        print('You win!')
+        user_wins +=1
+    elif not get_winner(cpu, user_class.wins):
+        print('You lose.')
+        cpu_wins +=1
+
+
+    # 6: Play again?
+
+    play_again = input('Play again? Enter "Y" or "N":\n')
+
+
+print(f'Player wins: {user_wins}')
+print(f'CPU wins: {cpu_wins}')
+
