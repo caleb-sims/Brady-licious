2f78cf3dda17beab84f3f956ec03048c036e4486
caleb-sims <caleb.sims@students.fhu.edu>
2025-04-29 09:42:24 -0500

    created Game and User classes
    Created structure. Details need to be filled in.

--- game.py
+++ game.py
@@ -1,3 +1,20 @@
+# import other classes
+
+d_options = {
+            'Rock':['Scissors', 'Lizard']
+
+
+
+
+        }
+
 class Game():
     def __init__(self):
+        # create gameplay
+
+    def winner(self):
+        # get winner from gameplay
+
+    def loop(self):
+        # set up different inputs for the users and play the game again
         
---
+++ user.py
@@ -0,0 +1,4 @@
+class User():
+    def __init__(self):
+        # get user input from the user
+        
